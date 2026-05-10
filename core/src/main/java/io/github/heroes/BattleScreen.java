40e3099d8df1f1a49fa1e82aff9e6084708e398c
bohun <bohun2van@gmail.com>
2026-05-10 21:31:01 +0200

    add BattleScreen link LobbyScreen and BattleScreen

--- core/src/main/java/io/github/heroes/BattleScreen.java
+++ core/src/main/java/io/github/heroes/BattleScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -12,53 +11,42 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-public class LobbyScreen extends ScreenAdapter {
+public class BattleScreen extends ScreenAdapter{
     private final Main game;
     private Stage stage;
     private Skin skin;
-    public LobbyScreen(Main game) {
+    public BattleScreen(Main game) {
         this.game = game;
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
         skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
         setupUI();
     }
-
     private void setupUI() {
         Table table = new Table();
         table.setFillParent(true);
         stage.addActor(table);
-        TextButton startButton = new TextButton("Start Game", skin);
-        TextButton exitButton = new TextButton("Exit", skin);
-        startButton.addListener(new ClickListener() {
+        TextButton backButton = new TextButton("Back to lobby", skin);
+        backButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y){
-                game.setScreen(new BattleScreen(game));
+                game.setScreen(new LobbyScreen(game));
             }
         });
-        exitButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y){
-                Gdx.app.exit();
-            }
-        });
-        table.add(startButton).expandX().fillX().uniform().pad(10);
-        table.row();
-        table.add(exitButton).expandX().fillX().uniform().pad(10);
+        table.add(backButton).expandX().fillX().uniform().pad(10);
+
     }
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+
+    public void render(float delta){
+        Gdx.gl.glClearColor(0.1f, 0.4f, 0.1f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
     }
-    @Override
-    public void resize(int width, int height) {
+    public void resize(int width, int height){
         stage.getViewport().update(width, height, true);
     }
-    @Override
-    public void dispose() {
+    public void dispose(){
         stage.dispose();
         skin.dispose();
     }
