1554673de7eda9a26a52eedff7e5df54eaa26e10
bohun <bohun2van@gmail.com>
2026-06-09 18:14:03 +0200

    add popup background

--- core/src/main/java/io/github/heroes/view/Battle/UnitInfoPopup.java
+++ core/src/main/java/io/github/heroes/view/Battle/UnitInfoPopup.java
@@ -1,27 +1,44 @@
 package io.github.heroes.view.Battle;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import io.github.heroes.model.state.UnitStack;
 
 public class UnitInfoPopup {
+    private static final float POPUP_WIDTH = 320f;
+    private static final float POPUP_HEIGHT = 300f;
+
     private final Table root;
     private final Table contentTable;
     private final Skin skin;
+    private final Texture popupBackground;
 
     public UnitInfoPopup(Skin skin) {
         this.skin = skin;
+        popupBackground = new Texture(Gdx.files.internal("Menu/popup_bg.png"));
+
         root = new Table();
         root.setFillParent(true);
         root.center();
         root.setVisible(false);
 
-        contentTable = new Table(skin);
-        contentTable.setBackground("window");
+        TextureRegionDrawable popupDrawable = new TextureRegionDrawable(popupBackground);
+        popupDrawable.setMinWidth(0f);
+        popupDrawable.setMinHeight(0f);
 
-        root.add(contentTable).width(320f).pad(24);
+        contentTable = new Table(skin);
+        contentTable.top();
+        contentTable.setBackground(popupDrawable);
+
+        root.add(contentTable)
+            .width(POPUP_WIDTH)
+            .height(POPUP_HEIGHT)
+            .pad(24);
     }
 
     public void addTo(Stage stage) {
@@ -37,9 +54,16 @@ public class UnitInfoPopup {
         root.setVisible(false);
     }
 
+    public void dispose() {
+        popupBackground.dispose();
+    }
+
     private void updateContent(UnitStack unit) {
         contentTable.clear();
-        contentTable.pad(18);
+        contentTable.padTop(56f);
+        contentTable.padLeft(26f);
+        contentTable.padRight(26f);
+        contentTable.padBottom(28f);
         contentTable.defaults().left().pad(4);
 
         addInfoRow("Unit", unit.getType().name());
